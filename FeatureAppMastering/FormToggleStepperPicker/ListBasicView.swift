6942a96f0bc2cdd9735338ac1044a4407630687d
JohnKlenov <klenovptz@mail.ru>
2024-08-27 14:56:18 +0300

    Begin create tuttorial №13

---
+++ FeatureAppMastering/FormToggleStepperPicker/ListBasicView.swift
@@ -0,0 +1,18 @@
+//
+//  ListBasicView.swift
+//  SwiftUIProject
+//
+//  Created by Evgenyi on 27.08.24.
+//
+
+import SwiftUI
+
+struct ListBasicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListBasicView()
+}
---
+++ FeatureAppMastering/FormToggleStepperPicker/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  SwiftUIProject
+//
+//  Created by Evgenyi on 27.08.24.
+//
+
+import SwiftUI
+
+struct Model: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Model()
+}
